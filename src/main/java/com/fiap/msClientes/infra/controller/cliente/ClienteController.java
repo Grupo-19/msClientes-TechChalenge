43a2b11a16e1f0f3cfe4f8611762b8e218210afd
Deverlyn Christine <deverlyn.cgoncalves@gmail.com>
2024-12-02 17:10:03 -0300

    update

--- src/main/java/com/fiap/msClientes/infra/controller/cliente/ClienteController.java
+++ src/main/java/com/fiap/msClientes/infra/controller/cliente/ClienteController.java
@@ -2,6 +2,8 @@ package com.fiap.msClientes.infra.controller.cliente;
 
 import com.fiap.msClientes.application.usecases.cliente.*;
 import com.fiap.msClientes.entities.Cliente;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
+@Tag(name = "Clientes", description = "Endpoints para gerenciamento de clientes")
 public class ClienteController {
 
     private final AtualizarCliente atualizarCliente;
@@ -30,23 +33,27 @@ public class ClienteController {
     }
 
     @PutMapping
+    @Operation(summary = "Atualizar Cliente", description = "Altera os dados de um cliente existente.")
     public ClienteDTO atualizarCliente(@RequestBody ClienteDTO dto) {
         Cliente salvo = atualizarCliente.atualizarCliente(new Cliente(dto.id(), dto.nome(), dto.cpf(), dto.email(), dto.senha()));
         return new ClienteDTO(salvo.getId(), salvo.getNome(), salvo.getCpf(), salvo.getEmail(), salvo.getSenha());
     }
 
     @PostMapping
+    @Operation(summary = "Cadastrar Cliente", description = "Cadastra um novo cliente.")
     public ClienteDTO cadastrarCliente(@RequestBody ClienteDTO dto) {
         Cliente salvo = cadastrarCliente.cadastrarCliente(new Cliente(dto.nome(), dto.cpf(), dto.email(), dto.senha()));
         return new ClienteDTO(salvo.getId(), salvo.getNome(), salvo.getCpf(), salvo.getEmail(), salvo.getSenha());
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir Cliente", description = "Exclui um cliente.")
     public void excluirCliente(@PathVariable Long id) {
         excluirCliente.excluirCliente(id);
     }
 
     @GetMapping("/cpf/{cpf}")
+    @Operation(summary = "Consultar Cliente por CPF", description = "Consulta um cliente à partir de um CPF.")
     public ClienteDTO obterClientePorCpf(@PathVariable String cpf) {
         Cliente cliente = obterClientePorCpf.obterClientePorCpf(cpf);
         return new ClienteDTO(cliente.getId(), cliente.getNome(), cliente.getCpf(), cliente.getEmail(), cliente.getSenha());
@@ -54,12 +61,14 @@ public class ClienteController {
     }
 
     @GetMapping("/id/{id}")
+    @Operation(summary = "Consultar Cliente por ID", description = "Consulta um cliente à partir de um ID.")
     public ClienteDTO obterClientePorId(@PathVariable Long id) {
         Cliente cliente = obterClientePorId.obterClientePorId(id);
         return new ClienteDTO(cliente.getId(), cliente.getNome(), cliente.getCpf(), cliente.getEmail(), cliente.getSenha());
     }
 
     @GetMapping
+    @Operation(summary = "Listar Todos Clientes", description = "Lista todos os clientes registrados.")
     public List<ClienteDTO> obterTodosOsClientes() {
         List<ClienteDTO> clientesDTO = new ArrayList<>();
         obterTodosOsClientes.obterTodosOsClientes().forEach(v -> clientesDTO.add(new ClienteDTO(
@@ -72,6 +81,7 @@ public class ClienteController {
     }
 
     @GetMapping("/consultar/{id}")
+    @Operation(summary = "Validar Cliente", description = "Valida a existência de um cliente.")
     public Boolean validarCliente(@PathVariable Long id) {
         return validarCliente.validarCliente(id);
     }
